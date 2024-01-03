18ccac752c4c4b0a36f1c5c07ac240a1e022ff38
hassanin1971 <154602711+hassanin1971@users.noreply.github.com>
2024-01-03 22:54:56 +0200

    first commit

---
+++ src/page/dashboard/data.jsx
@@ -0,0 +1,134 @@
+export const data1 = [
+    {
+      id: "React",
+      label: "React",
+      value: 272,
+      color: "hsl(107, 70%, 50%)",
+    },
+    {
+      id: "stylus",
+      label: "stylus",
+      value: 543,
+      color: "hsl(64, 70%, 50%)",
+    },
+    {
+      id: "sass",
+      label: "sass",
+      value: 401,
+      color: "hsl(41, 70%, 50%)",
+    },
+    {
+      id: "haskell",
+      label: "haskell",
+      value: 434,
+      color: "hsl(172, 70%, 50%)",
+    },
+    {
+      id: "nue",
+      label: "nue",
+      value: 333,
+      color: "hsl(219, 70%, 50%)",
+    },
+  ];
+
+  export const data2 = [
+    {
+      id: "React",
+      label: "React",
+      value: 272,
+      color: "hsl(107, 70%, 50%)",
+    },
+    {
+      id: "stylus",
+      label: "stylus",
+      value: 543,
+      color: "hsl(64, 70%, 50%)",
+    },
+    {
+      id: "sass",
+      label: "sass",
+      value: 401,
+      color: "hsl(41, 70%, 50%)",
+    },
+    {
+      id: "haskell",
+      label: "haskell",
+      value: 434,
+      color: "hsl(172, 70%, 50%)",
+    },
+    {
+      id: "nue",
+      label: "nue",
+      value: 333,
+      color: "hsl(219, 70%, 50%)",
+    },
+  ];
+
+
+
+  export const data3 = [
+    {
+      id: "React",
+      label: "React",
+      value: 272,
+      color: "hsl(107, 70%, 50%)",
+    },
+    {
+      id: "stylus",
+      label: "stylus",
+      value: 543,
+      color: "hsl(64, 70%, 50%)",
+    },
+    {
+      id: "sass",
+      label: "sass",
+      value: 401,
+      color: "hsl(41, 70%, 50%)",
+    },
+    {
+      id: "haskell",
+      label: "haskell",
+      value: 434,
+      color: "hsl(172, 70%, 50%)",
+    },
+    {
+      id: "nue",
+      label: "nue",
+      value: 333,
+      color: "hsl(219, 70%, 50%)",
+    },
+  ];
+
+
+  export const data4 = [
+    {
+      id: "React",
+      label: "React",
+      value: 272,
+      color: "hsl(107, 70%, 50%)",
+    },
+    {
+      id: "stylus",
+      label: "stylus",
+      value: 543,
+      color: "hsl(64, 70%, 50%)",
+    },
+    {
+      id: "sass",
+      label: "sass",
+      value: 401,
+      color: "hsl(41, 70%, 50%)",
+    },
+    {
+      id: "haskell",
+      label: "haskell",
+      value: 434,
+      color: "hsl(172, 70%, 50%)",
+    },
+    {
+      id: "nue",
+      label: "nue",
+      value: 333,
+      color: "hsl(219, 70%, 50%)",
+    },
+  ];
